b4d01cd9082e01320bea929d7293d367ef6160fe
jonutefe840 <nunoferreira840@hotmail.com>
2019-06-04 17:36:59 +0100

    first commit Ex2

---
+++ EX2/ex2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from scipy.optimize import minimize
+
+from sklearn.preprocessing import PolynomialFeatures
+
+pd.set_option('display.notebook_repr_html', False)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 150)
+pd.set_option('display.max_seq_items', None)
+
+import seaborn as sns
+sns.set_context('notebook')
+sns.set_style('white')
