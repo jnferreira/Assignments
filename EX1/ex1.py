3c536ab1b3aa2ece1167383a4db672a3110e8755
jonutefe840 <nunoferreira840@hotmail.com>
2019-06-04 15:47:06 +0100

    comparing Scikit-learn with Gradient descent

--- EX1/ex1.py
+++ EX1/ex1.py
@@ -29,28 +29,38 @@ data = np.loadtxt('ex1data1.txt', delimiter=',')
 X = np.c_[np.ones(data.shape[0]),data[:,0]]
 y = np.c_[data[:,1]]
 
-plt.scatter(X[:,1], y, s=50, c='r', marker='x', linewidths=1)
-plt.xlim(4.5,26)
+# plt.scatter(X[:,1], y, s=50, c='r', marker='x', linewidths=1)
+# plt.xlim(4.5,26)
 
-# name x and y
+# # name x and y
+# plt.xlabel('Population of City in 10,000s')
+# plt.ylabel('Profit in $10,000s')
+
+theta , Cost_J = gradientDescent.gradientDescent(X, y)
+# print('theta: ',theta.ravel())
+
+# plt.plot(Cost_J)
+# plt.ylabel('Cost J')
+# plt.xlabel('Iterations')
+# plt.show()
+
+
+xx = np.arange(5,23)
+yy = theta[0]+theta[1]*xx
+
+# Plot gradient descent
+plt.scatter(X[:,1], y, s=30, c='r', marker='x', linewidths=1)
+plt.plot(xx,yy, label='Linear regression (Gradient descent)')
+
+# Compare with Scikit-learn Linear regression 
+regr = LinearRegression()
+regr.fit(X[:,1].reshape(-1,1), y.ravel())
+plt.plot(xx, regr.intercept_+regr.coef_*xx, label='Linear regression (Scikit-learn GLM)')
+
+plt.xlim(4,24)
 plt.xlabel('Population of City in 10,000s')
 plt.ylabel('Profit in $10,000s')
-
-#plt.show()
-
-#theta , Cost_J = gradientDescent(X, y)
-#print('theta: ',theta.ravel())
-
-#plt.plot(Cost_J)
-#plt.ylabel('Cost J')
-#plt.xlabel('Iterations')
+plt.legend(loc=4)
+plt.show()
 
 
-#plt.show()
-
-theta , Cost_J = gradientDescent(X, y)
-print('theta: ',theta.ravel())
-
-plt.plot(Cost_J)
-plt.ylabel('Cost J')
-plt.xlabel('Iterations');
