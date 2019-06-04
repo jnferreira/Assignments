de7ea39923484ec01a3ca47c66dcd4c1851db215
jonutefe840 <nunoferreira840@hotmail.com>
2019-06-04 14:57:21 +0100

    imports

--- EX1/computeCost.py
+++ EX1/computeCost.py
@@ -1,12 +1,15 @@
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 
 def computeCost(X, y, theta=[[0],[0]]):
     m = y.size
     J = 0
-
+    
     h = X.dot(theta)
 
     J = 1/(2*m)*np.sum(np.square(h-y))
-
+    
     return(J)
 
--- EX1/ex1.py
+++ EX1/ex1.py
@@ -1,10 +1,11 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-
-from sklearn.linear_model import LinearRegression
+import numpy as np
+import pandas as pd
+import seaborn as sns
 #from mpl_toolkits.mplot3dp import axes3d
 from computeCost import computeCost
+from gradientDescent import gradientDescent
+from sklearn.linear_model import LinearRegression
 
 pd.set_option('display.notebook_repr_html', False)
 pd.set_option('display.max_columns', None)
@@ -12,7 +13,6 @@ pd.set_option('display.max_rows', 150)
 pd.set_option('display.max_seq_items', None)
 
 
-import seaborn as sns
 sns.set_context('notebook')
 sns.set_style('white')
 
@@ -27,16 +27,23 @@ data = np.loadtxt('ex1data1.txt', delimiter=',')
 X = np.c_[np.ones(data.shape[0]),data[:,0]]
 y = np.c_[data[:,1]]
 
-plt.scatter(X[:,1], y, s=30, c='r', marker='x', linewidths=1)
-plt.xlim(4,24)
+plt.scatter(X[:,1], y, s=50, c='r', marker='x', linewidths=1)
+plt.xlim(4.5,26)
 
 # name x and y
 plt.xlabel('Population of City in 10,000s')
 plt.ylabel('Profit in $10,000s')
 
-# plt.show()
+#plt.show()
 
-print(computeCost(X, y))
+theta , Cost_J = gradientDescent(X, y)
+print('theta: ',theta.ravel())
+
+plt.plot(Cost_J)
+plt.ylabel('Cost J')
+plt.xlabel('Iterations')
 
 
+#plt.show()
 
+computeCost(X, y)
