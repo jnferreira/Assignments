d79ee93e4794e3545e1a9eea7616bf09b675b930
jonutefe840 <nunoferreira840@hotmail.com>
2019-06-04 12:31:39 +0100

    computeCost function

--- EX1/ex1.py
+++ EX1/ex1.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LinearRegression
 #from mpl_toolkits.mplot3dp import axes3d
+from computeCost import computeCost
 
 pd.set_option('display.notebook_repr_html', False)
 pd.set_option('display.max_columns', None)
@@ -17,6 +18,7 @@ sns.set_style('white')
 
 def warmUpExercise():
     return(np.identity(10))
+    
 
 warmUpExercise()
 
@@ -24,3 +26,17 @@ data = np.loadtxt('ex1data1.txt', delimiter=',')
 
 X = np.c_[np.ones(data.shape[0]),data[:,0]]
 y = np.c_[data[:,1]]
+
+plt.scatter(X[:,1], y, s=30, c='r', marker='x', linewidths=1)
+plt.xlim(4,24)
+
+# name x and y
+plt.xlabel('Population of City in 10,000s')
+plt.ylabel('Profit in $10,000s')
+
+# plt.show()
+
+print(computeCost(X, y))
+
+
+
