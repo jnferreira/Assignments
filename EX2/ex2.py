cf1ece7747e1ea0f3774892e1fc0a2892050aa31
jonutefe840 <nunoferreira840@hotmail.com>
2019-06-05 09:24:24 +0100

    commit ex2

--- EX2/ex2.py
+++ EX2/ex2.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
 import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import plotdata
 
 from scipy.optimize import minimize
 
@@ -15,3 +17,12 @@ pd.set_option('display.max_seq_items', None)
 import seaborn as sns
 sns.set_context('notebook')
 sns.set_style('white')
+
+data = np.loadtxt('ex2data1.txt', delimiter=',')
+
+X = np.c_[np.ones((data.shape[0],1)), data[:,0:2]]
+y = np.c_[data[:,2]]
+
+#print (np.ones((data.shape[0],1)), data[:,0:2])
+
+plotdata.plotData(data, 'Exam 1 score', 'Exam 2 score', 'Admitted', 'Not admitted')
---
+++ EX2/plotdata.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from scipy.optimize import minimize
+
+from sklearn.preprocessing import PolynomialFeatures
+
+def plotData(data, label_x, label_y, label_pos, label_neg, axes=None):
+    # Get indexes for class 0 and class 1
+    neg = data[:,2] == 0
+    pos = data[:,2] == 1
+    
+    # If no specific axes object has been passed, get the current axes.
+    if axes == None:
+        axes = plt.gca()
+    axes.scatter(data[pos][:,0], data[pos][:,1], marker='+', c='k', s=60, linewidth=2, label=label_pos)
+    axes.scatter(data[neg][:,0], data[neg][:,1], c='y', s=60, label=label_neg)
+    axes.set_xlabel(label_x)
+    axes.set_ylabel(label_y)
+    axes.legend(frameon= True, fancybox = True)
