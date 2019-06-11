cc892d2913356a1364b820530a8a12b4787e6650
jonutefe840 <nunoferreira840@hotmail.com>
2019-06-11 09:09:04 +0100

    ex7 final commit

--- EX7/ex7.py
+++ EX7/ex7.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import functions
 
-from scipy.optimize import minimize
-
-from sklearn.preprocessing import PolynomialFeatures
+from scipy.io import loadmat
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from scipy import linalg
 
 pd.set_option('display.notebook_repr_html', False)
 pd.set_option('display.max_columns', None)
@@ -18,5 +18,62 @@ import seaborn as sns
 sns.set_context('notebook')
 sns.set_style('white')
 
-data = functions.loaddata('ex2data1.txt', ',')
+data1 = loadmat('ex7data2.mat')
+data1.keys()
 
+X1 = data1['X']
+print('X1:', X1.shape)
+
+km1 = KMeans(3)
+km1.fit(X1)
+
+# plt.scatter(X1[:,0], X1[:,1], s=40, c=km1.labels_, cmap=plt.cm.prism) 
+# plt.title('K-Means Clustering Results with K=3')
+# plt.scatter(km1.cluster_centers_[:,0], km1.cluster_centers_[:,1], marker='+', s=100, c='k', linewidth=2)
+# #plt.show()
+
+img = plt.imread('bird_small.png')
+img_shape = img.shape
+img_shape
+
+A = img/255
+
+AA = A.reshape(128*128,3)
+AA.shape
+
+km2 = KMeans(5)
+km2.fit(AA)
+
+B = km2.cluster_centers_[km2.labels_].reshape(img_shape[0], img_shape[1], 3)
+
+# fig, (ax1, ax2) = plt.subplots(1,2, figsize=(13,9))
+# ax1.imshow(img)
+# ax1.set_title('Original')
+# ax2.imshow(B*255)
+# ax2.set_title('Compressed, with 16 colors')
+
+# for ax in fig.axes:
+#     ax.axis('off')
+
+#plt.show()
+
+data2 = loadmat('ex7data1.mat')
+data2.keys()
+
+X2 = data2['X']
+print('X2:', X2.shape)
+
+scaler = StandardScaler()
+scaler.fit(X2)
+
+U, S, V = linalg.svd(scaler.transform(X2).T)
+print(U)
+print(S)
+
+plt.scatter(X2[:,0], X2[:,1], s=30, edgecolors='b',facecolors='None', linewidth=1)
+# setting aspect ratio to 'equal' in order to show orthogonality of principal components in the plot
+plt.gca().set_aspect('equal')
+plt.quiver(scaler.mean_[0], scaler.mean_[1], U[0,0], U[0,1], scale=S[1], color='r')
+plt.quiver(scaler.mean_[0], scaler.mean_[1], U[1,0], U[1,1], scale=S[0], color='r')
+
+plt.show()
--- EX7/functions.py
+++ EX7/functions.py
@@ -1,30 +1,22 @@
+#!/usr/bin/python3
 import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-from scipy.optimize import minimize
+from scipy.io import loadmat
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from scipy import linalg
 
-from sklearn.preprocessing import PolynomialFeatures
+pd.set_option('display.notebook_repr_html', False)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 150)
+pd.set_option('display.max_seq_items', None)
+ 
+import seaborn as sns
+sns.set_context('notebook')
+sns.set_style('white')
 
 
-def loaddata(file, delimeter):
-    data = np.loadtxt(file, delimiter=delimeter)
-    print('Dimensions: ',data.shape)
-    print(data[1:6,:])
-    return(data)
-
-def plotData(data, label_x, label_y, label_pos, label_neg, axes=None):
-    # Get indexes for class 0 and class 1
-    neg = data[:,2] == 0
-    pos = data[:,2] == 1
-    
-    # If no specific axes object has been passed, get the current axes.
-    if axes == None:
-        axes = plt.gca()
-    axes.scatter(data[pos][:,0], data[pos][:,1], marker='+', c='k', s=60, linewidth=2, label=label_pos)
-    axes.scatter(data[neg][:,0], data[neg][:,1], c='y', s=60, label=label_neg)
-    axes.set_xlabel(label_x)
-    axes.set_ylabel(label_y)
-    axes.legend(frameon= True, fancybox = True)
 
